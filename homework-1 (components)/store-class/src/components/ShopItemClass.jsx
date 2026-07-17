12c31c5d8ef742ac4578f20a5d1b8fd7111973b6
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-07-17 19:16:22 +0300

    dz 1.2

---
+++ homework-1 (components)/store-class/src/components/ShopItemClass.jsx
@@ -0,0 +1,23 @@
+import {Component} from "react";
+export default class ShopItem extends Component {
+    render() {
+        const {item} = this.props;
+        const getPrice = () => item.currency + item.price.toFixed(2);
+        return (
+            <div className="main-content">
+                <h2> {item.brand} </h2>
+                <h1> {item.title} </h1>
+                <h3> {item.description} </h3>
+                <div className="description">{item.descriptionFull}</div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{getPrice()}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        )
+    }
+}
