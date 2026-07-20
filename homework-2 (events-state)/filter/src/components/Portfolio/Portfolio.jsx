0e2934e9685dfef167ce8eeea4ad1c5b1a116a5a
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-07-20 12:17:08 +0300

    fix

--- homework-2 (events-state)/filter/src/components/Portfolio/Portfolio.jsx
+++ homework-2 (events-state)/filter/src/components/Portfolio/Portfolio.jsx
@@ -5,18 +5,19 @@ import ProjectList from "../ProjectList/ProjectList.jsx";
 import "./Portfolio.css";
 
 export default class Portfolio extends Component {
+    filters = ["All", "Websites", "Flayers", "Business Cards"]
+    projects = listProjectData
     state = {
         activeFilter: "All"
     }
 
     render () {
-        const filters = ["All", "Websites", "Flayers", "Business Cards"]
-        const selectedProjects = this.state.activeFilter === "All" ? listProjectData : listProjectData.filter((project) => (project.category === this.state.activeFilter))
+        const selectedProjects = this.state.activeFilter === "All" ? this.projects : this.projects.filter((project) => (project.category === this.state.activeFilter))
 
         return (
             <div className="portfolio">
                 <Toolbar
-                    filters={filters}
+                    filters={this.filters}
                     selected={this.state.activeFilter}
                     onSelectFilter={(filter) => this.setState({activeFilter: filter})}/>
                 <ProjectList
