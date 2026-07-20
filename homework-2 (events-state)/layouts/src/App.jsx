87cee4b9a6a6e107295e5b74ace36920f2a5e9f5
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-07-20 13:28:26 +0300

    change files and add vite

---
+++ homework-2 (events-state)/layouts/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <>
+        <button className="btn btn-outline btn-primary">Primary</button>
+      </>
+  )
+}
+
+export default App
