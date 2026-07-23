df880b1cd769424ecc603ed401c990052e9efe07
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-07-23 22:18:57 +0300

    add Offer type and configure json and reformat

--- homework-3(props)/listing/models/OfferModels.tsx
+++ homework-3(props)/listing/models/OfferModels.tsx
@@ -1,11 +1,11 @@
 export type Offer = {
-    listing_id: number,
-    url: string,
-    MainImage: {
-        url_570xN: string;
-    };
-    title: string,
-    currency_code: string;
-    price: string,
-    quantity: number,
-}
+  listing_id: number;
+  url: string;
+  MainImage: {
+    url_570xN: string;
+  };
+  title: string;
+  currency_code: string;
+  price: string;
+  quantity: number;
+};
