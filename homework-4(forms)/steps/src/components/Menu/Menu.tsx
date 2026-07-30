3b415b908f38fae10aa6fc340b62f4096a15672f
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-07-30 18:54:57 +0300

    add input form

--- homework-4(forms)/steps/src/components/Menu/Menu.tsx
+++ homework-4(forms)/steps/src/components/Menu/Menu.tsx
@@ -1,13 +1,38 @@
 import WorkoutList from "../WorkoutList/WorkoutList.tsx";
 import "./Menu.css";
+import { useState } from "react";
+import * as React from "react";
 function Menu() {
+  const [formData, setFormData] = useState({ date: "", distance: "" });
+  //const [workoutData, setWorkoutData] = useState<Map<string, number>>(new Map());
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className="menu">
-      <form className="add-date">
+      <form className="add-date" onSubmit={handleSubmit}>
         <label htmlFor="date">Дата (ДД.ММ.ГГГГ)</label>
-        <input name="date" type="text" />
+        <input
+          name="date"
+          type="text"
+          value={formData.date}
+          onChange={handleChange}
+        />
         <label htmlFor="distance">Пройдено км</label>
-        <input name="distance" type="text" />
+        <input
+          name="distance"
+          type="text"
+          value={formData.distance}
+          onChange={handleChange}
+        />
         <button type="submit">OK</button>
       </form>
       <WorkoutList />
