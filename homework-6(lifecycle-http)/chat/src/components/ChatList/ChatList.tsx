b89fd14a20db8e08da2bad8c0570f86049cf5435
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-08-12 21:26:39 +0300

    add autoscroll

--- homework-6(lifecycle-http)/chat/src/components/ChatList/ChatList.tsx
+++ homework-6(lifecycle-http)/chat/src/components/ChatList/ChatList.tsx
@@ -1,13 +1,19 @@
 import type { ChatType } from "../../utils/types.ts";
 import Chat from "../Chat/Chat.tsx";
 import "./ChatList.css"
+import {useEffect, useRef} from "react";
 
 function ChatList({ list, userId }: { list: ChatType[], userId: string }) {
+  const bottomRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [list]);
   return (
     <div className="chat-list">
       {list.map((chat: ChatType) => (
         <Chat key={chat.id} chat={chat} userId={userId} />
       ))}
+      <div ref={bottomRef} />
     </div>
   );
 }
