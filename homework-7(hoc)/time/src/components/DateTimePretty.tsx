13830b45f2a58d9231545790cd57e2acac8057ea
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-08-21 16:03:27 +0300

    add reformate date

--- homework-7(hoc)/time/src/components/DateTimePretty.tsx
+++ homework-7(hoc)/time/src/components/DateTimePretty.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import DateTime from "./DateTime.tsx";
-import moment from "moment";
+import moment from "moment/min/moment-with-locales";
+moment.locale("ru");
+moment.relativeTimeThreshold("m", 60);
+moment.relativeTimeThreshold("h", 24);
+moment.relativeTimeThreshold("d", 10000);
 
 function withTime(Component: React.ComponentType<{ date: string }>) {
   return function DateTimePretty({ date }: { date: string }) {
--- homework-7(hoc)/time/src/utils/videos.ts
+++ homework-7(hoc)/time/src/utils/videos.ts
@@ -3,7 +3,7 @@ import type { VideoType } from "./types.ts";
 export const videos: VideoType[] = [
   {
     url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
-    date: "2015-07-31 13:24:00",
+    date: "2025-07-31 13:24:00",
   },
   {
     url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
@@ -11,7 +11,7 @@ export const videos: VideoType[] = [
   },
   {
     url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
-    date: "2018-02-03 23:16:00",
+    date: "2026-02-03 23:16:00",
   },
   {
     url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
