fcc816ebb3d6ca0664bf1c3c04a30b2aef6743e0
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-08-23 13:53:09 +0300

    add noc

--- homework-7(hoc)/highlight/src/components/NewPopularBlock.tsx
+++ homework-7(hoc)/highlight/src/components/NewPopularBlock.tsx
@@ -5,7 +5,7 @@ import New from "./New.tsx";
 function NewPopularBlock(Component: React.ComponentType<BlockType>) {
   return function BlockPretty(block: BlockType) {
     const content = <Component {...block} />;
-    return block.views > 1000 ? (
+    return block.views >= 1000 ? (
       <Popular> {content} </Popular>
     ) : block.views < 100 ? (
       <New> {content} </New>
