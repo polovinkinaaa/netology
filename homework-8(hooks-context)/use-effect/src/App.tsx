3a116ac9abf6a18535c0eef23da437c403f96304
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-09-02 13:41:16 +0300

    add functional for loading all list of users

--- homework-8(hooks-context)/use-effect/src/App.tsx
+++ homework-8(hooks-context)/use-effect/src/App.tsx
@@ -1,7 +1,34 @@
 import "./App.css";
+import List from "./components/List.tsx";
+import Details from "./components/Details.tsx";
+import { useEffect, useState } from "react";
+
+const url = import.meta.env.VITE_USERS_URL;
 
 function App() {
-  return <div className="App">app</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Ошибка сети");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <List users={users} />
+      <Details />
+    </div>
+  );
 }
 
 export default App;
---
+++ homework-8(hooks-context)/use-effect/src/components/Details.tsx
@@ -0,0 +1,5 @@
+function Details() {
+  return <div className="details">details</div>;
+}
+
+export default Details;
