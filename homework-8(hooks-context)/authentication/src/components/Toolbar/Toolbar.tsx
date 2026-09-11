4e8ca6ff4de33fefe49b00c7130068543c789d7b
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-09-11 16:40:29 +0300

    add user toolbar

--- homework-8(hooks-context)/authentication/src/components/Toolbar/Toolbar.tsx
+++ homework-8(hooks-context)/authentication/src/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,21 @@
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext.tsx";
+
 function Toolbar() {
-  return <div className="toolbar"> toolbar </div>;
+  const { profile, setToken, setProfile } = useContext(AuthContext);
+  const handleDelete = () => {
+    setToken(null);
+    setProfile(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("profile");
+  };
+  return (
+    <div className="toolbar">
+      <div className="website-name">Neto Social</div>
+      <div className="user-name"> Hello, {profile?.name} </div>
+      <img className="user-avatar" src={profile?.avatar} alt="avatar" />
+      <button onClick={handleDelete}>Logout</button>
+    </div>
+  );
 }
 export default Toolbar;
