dbdec6619331e2c7348f87fe5a344da197f35071
polovinkinaaa <polovinkina.aa@phystech.edu>
2026-09-11 17:49:03 +0300

    add loading from localStorage

--- homework-8(hooks-context)/authentication/src/App.tsx
+++ homework-8(hooks-context)/authentication/src/App.tsx
@@ -7,8 +7,13 @@ import News from "./components/News/News.tsx";
 import type { Profile } from "./types.ts";
 
 function App() {
-  const [token, setToken] = useState<string | null>(null);
-  const [profile, setProfile] = useState<Profile | null>(null);
+  const [token, setToken] = useState<string | null>(() =>
+    localStorage.getItem("token"),
+  );
+  const [profile, setProfile] = useState<Profile | null>(() => {
+    const raw = localStorage.getItem("profile");
+    return raw ? JSON.parse(raw) : null;
+  });
 
   return (
     <AuthContext.Provider value={{ token, setToken, profile, setProfile }}>
